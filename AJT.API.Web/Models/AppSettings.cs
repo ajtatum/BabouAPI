889303be0141ebb48afeb2cde68bdf221b83f9c5
AJ Tatum <andrew.j.tatum@gmail.com>
2019-12-02 11:40:36 -0500

    Updated the ordering of classes based on secrets. OCD

--- AJT.API.Web/Models/AppSettings.cs
+++ AJT.API.Web/Models/AppSettings.cs
@@ -2,25 +2,16 @@
 {
     public class AppSettings
     {
-        public string IpStackApiKey { get; set; }
-        public string BuildNumber { get; set; }
         public string AdminSafeList { get; set; }
-        public EmailSenderSettings EmailSender { get; set; }
         public ApplicationInsightsSettings ApplicationInsights { get; set; }
         public AuthenticationSettings Authentication { get; set; }
+        public string BuildNumber { get; set; }
+        public EmailSenderSettings EmailSender { get; set; }
+        public GoogleCustomSearchSettings GoogleCustomSearch { get; set; }
+        public string IpStackApiKey { get; set; }
         public PushBulletSettings PushBullet { get; set; }
         public SlackSettings Slack { get; set; }
-        public GoogleCustomSearchSettings GoogleCustomSearch { get; set; }
     }
-
-    public class EmailSenderSettings
-    {
-        public string FromSenderName { get; set; }
-        public string FromUserName { get; set; }
-        public string Domain { get; set; }
-        public string ApiKey { get; set; }
-    }
-
 
     public class ApplicationInsightsSettings
     {
@@ -29,9 +20,15 @@
 
     public class AuthenticationSettings
     {
+        public AuthFacebook Facebook { get; set; }
         public AuthGoogle Google { get; set; }
         public MicrosoftAuth Microsoft { get; set; }
-        public AuthFacebook Facebook { get; set; }
+
+        public class AuthFacebook
+        {
+            public string AppId { get; set; }
+            public string AppSecret { get; set; }
+        }
 
         public class AuthGoogle
         {
@@ -44,12 +41,21 @@
             public string ClientId { get; set; }
             public string ClientSecret { get; set; }
         }
+    }
 
-        public class AuthFacebook
-        {
-            public string AppId { get; set; }
-            public string AppSecret { get; set; }
-        }
+    public class EmailSenderSettings
+    {
+        public string ApiKey { get; set; }
+        public string Domain { get; set; }
+        public string FromSenderName { get; set; }
+        public string FromUserName { get; set; }
+    }
+
+    public class GoogleCustomSearchSettings
+    {
+        public string ApiKey { get; set; }
+        public string DcComicsCx { get; set; }
+        public string MarvelCx { get; set; }
     }
 
     public class PushBulletSettings
@@ -57,17 +63,11 @@
         public string ApiKey { get; set; }
         public string EncryptionKey { get; set; }
     }
+
     public class SlackSettings
     {
         public string ApiToken { get; set; }
         public string SigningSecret { get; set; }
         public string VerificationToken { get; set; }
-    }
-
-    public class GoogleCustomSearchSettings
-    {
-        public string ApiKey { get; set; }
-        public string MarvelCx { get; set; }
-        public string DcComicsCx { get; set; }
     }
 }
