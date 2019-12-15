86e9345aaf3db794eebd7fa590e5786d8c9c8c3b
AJ Tatum <andrew.j.tatum@gmail.com>
2019-12-15 18:48:21 -0500

    Fixed login error

--- AJT.API.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ AJT.API.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -82,7 +82,15 @@ namespace AJT.API.Web.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: true);
+                var user = await _userManager.FindByEmailAsync(Input.Email);
+
+                if (user == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return Page();
+                }
+
+                var result = await _signInManager.PasswordSignInAsync(user.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
