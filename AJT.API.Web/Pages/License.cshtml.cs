caa8d9590fa8038820e97b3826692c332d3f6484
AJ Tatum <andrew.j.tatum@gmail.com>
2019-12-23 16:16:50 -0500

    Added Terms of Service and License

---
+++ AJT.API.Web/Pages/License.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AJT.API.Web.Pages
+{
+    public class LicenseModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ AJT.API.Web/Pages/TermsOfService.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AJT.API.Web.Pages
+{
+    public class TermsOfServiceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
