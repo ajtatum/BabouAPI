b5cadcc19bf6834f5cee1216ede48c3c7a12543d
AJ Tatum <andrew.j.tatum@gmail.com>
2019-12-23 21:15:50 -0500

    Working on Contact, etc pages

--- AJT.API.Web/Helpers/ExtensionMethods/UserManagerExtensions.cs
+++ AJT.API.Web/Helpers/ExtensionMethods/UserManagerExtensions.cs
@@ -1,31 +1,65 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using AJT.API.Web.Models.Database;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Serilog;
 
 namespace AJT.API.Web.Helpers.ExtensionMethods
 {
     public static class UserManagerExtensions
     {
         private static IHttpContextAccessor _httpContextAccessor;
+        private static ILogger _logger;
 
         public static void Configure(IHttpContextAccessor httpContextAccessor)
         {
             _httpContextAccessor = httpContextAccessor;
+            _logger = Log.ForContext(typeof(UserManagerExtensions));
         }
 
         public static HttpContext Current => _httpContextAccessor.HttpContext;
 
         public static ApplicationUser FindByApiAuthKeyAsync(this UserManager<ApplicationUser> um, string apiAuthKey)
         {
-            return um?.Users?.FirstOrDefault(x=>x.ApiAuthKey == apiAuthKey);
+            try
+            {
+                return um.Users.FirstOrDefault(x => x.ApiAuthKey == apiAuthKey);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "UserManagerExtensions: Error finding user by Api AuthKey: {AuthKey}.", apiAuthKey);
+                return null;
+            }
         }
 
         public static async Task<string> GetApiAuthKeyAsync(this UserManager<ApplicationUser> um)
         {
-            var user = await um?.GetUserAsync(Current.User);
-            return user?.ApiAuthKey;
+            try
+            {
+                var user = await um.GetUserAsync(_httpContextAccessor.HttpContext.User);
+                return user.ApiAuthKey;
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "UserManagerExtensions: Error retrieving user ApiAuthKey.");
+                return string.Empty;
+            }
+        }
+
+        public static async Task<ApplicationUser> GetCurrentUserAsync(this UserManager<ApplicationUser> um)
+        {
+            try
+            {
+                var user = await um.GetUserAsync(_httpContextAccessor.HttpContext.User);
+                return user;
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "UserManagerExtensions: Error getting current user.");
+                return null;
+            }
         }
     }
 }
