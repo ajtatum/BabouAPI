cca7f614222c2682d28dd4296df030b103005786
AJ Tatum <andrew.j.tatum@gmail.com>
2020-01-04 21:58:03 -0500

    Added CreateMarvelShortUrl mainly to quickly create short urls from Slack, Telegram, etc with just an email address

--- Babou.API.Web/Areas/API/UrlShortenerController.cs
+++ Babou.API.Web/Areas/API/UrlShortenerController.cs
@@ -34,14 +34,16 @@ namespace Babou.API.Web.Areas.API
         private readonly IUrlShortenerService _urlShortenerService;
         private readonly ILogger<UrlShortenerController> _logger;
         private readonly AppSettings _appSettings;
+        private readonly IUserService _userService;
 
         public UrlShortenerController(ApplicationDbContext context, IUrlShortenerService urlShortenerService,
-            ILogger<UrlShortenerController> logger, IOptionsMonitor<AppSettings> appSettings)
+            ILogger<UrlShortenerController> logger, IOptionsMonitor<AppSettings> appSettings, IUserService userService)
         {
             _context = context;
             _urlShortenerService = urlShortenerService;
             _logger = logger;
             _appSettings = appSettings.CurrentValue;
+            _userService = userService;
         }
 
         /// <param name="longUrl">The URL you wish to shorten</param>
@@ -124,6 +126,47 @@ namespace Babou.API.Web.Areas.API
                 _logger.LogError(ex, "UrlShortenerController: Error creating short url for {LongUrl}, {Domain} and {Token}. Requested by {AuthKey}", longUrl, domainString, token, userAuthKey);
 
                 return new BadRequestObjectResult(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Creates or uses an existing account with the provided email address and creates a short URL.
+        /// <para>Once you've used this method, you can log into babou.io and do "Forgot Password" to login
+        /// and retrieve your AuthKey and have more control over your short url creation.
+        /// </para>
+        /// </summary>
+        /// <param name="longUrl"></param>
+        /// <param name="emailAddress"></param>
+        /// <returns></returns>
+        [ServiceFilter(typeof(AuthKeyFilter))]
+        [HttpPost("marvel")] 
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public async Task<IActionResult> CreateMarvelShortUrl([Required] [FromHeader] string longUrl, [Required] [FromHeader][EmailAddress] string emailAddress)
+        {
+            var userAuthKey = Request.Headers["AuthKey"].ToString();
+
+            try
+            {
+                var authKeyUser = await _context.ApplicationUsers.FirstOrDefaultAsync(x => x.ApiAuthKey == userAuthKey);
+
+                var applicationUser = await _context.ApplicationUsers.FirstOrDefaultAsync(x => x.Email == emailAddress) 
+                                      ?? await _userService.QuickCreateUser(emailAddress, null, null, null);
+
+                if(applicationUser == null)
+                    return new BadRequestObjectResult($"Error finding or creating user with email address {emailAddress}");
+
+                var domainString = Domains.MrvlCo.GetAttributeOfType<EnumMemberAttribute>().Value;
+
+                var shortenedUrl = await _urlShortenerService.CreateByUserId(applicationUser.Id, longUrl, domainString);
+
+                _logger.LogInformation("UrlShortenerController: CreateMarvelShortUrl - New Shortened Url Created for {LongUrl} as {ShortUrl}. Requested by {UserId} via {AuthKeyUserId}.", shortenedUrl.LongUrl, shortenedUrl.ShortUrl, applicationUser.Id, authKeyUser.Id);
+
+                return new OkObjectResult(shortenedUrl.ShortUrl);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "UrlShortenerController: CreateMarvelShortUrl - Error creating shortened url from email address.");
+                return new BadRequestObjectResult($"There was an error processing your request. Error: {ex.Message}");
             }
         }
     }
