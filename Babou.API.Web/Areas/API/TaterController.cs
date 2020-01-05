6a9401d080c5ef8ab3b42aeb5bd015bc55fca863
AJ Tatum <andrew.j.tatum@gmail.com>
2020-01-04 21:47:41 -0500

    Removed MaxMind test

--- Babou.API.Web/Areas/API/TaterController.cs
+++ Babou.API.Web/Areas/API/TaterController.cs
@@ -1,8 +1,6 @@
-﻿using System.Linq;
-using Babou.API.Web.Helpers.Filters;
+﻿using Babou.API.Web.Helpers.Filters;
 using Babou.API.Web.Models;
 using Babou.API.Web.Services.Interfaces;
-using MaxMind.GeoIP2;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -41,30 +39,6 @@ namespace Babou.API.Web.Areas.API
         public IActionResult RefreshKeyVault()
         {
             Startup.ConfigurationRoot.Reload();
-            return new OkResult();
-        }
-
-        [ServiceFilter(typeof(AuthKeyFilter))]
-        [HttpGet]
-        public IActionResult GetMaxMindIP(string ipAddress)
-        {
-            using var reader = new DatabaseReader(_appSettings.MaxMindDb);
-
-            var city = reader.City(ipAddress);
-
-            _logger.LogInformation("Country ISO Code: {CountryCode}", city.Country.IsoCode);
-            _logger.LogInformation("Country Name: {CountyName}", city.Country.Name);
-
-            _logger.LogInformation("Full State Name: {StateName}", city.MostSpecificSubdivision.Name);
-            _logger.LogInformation("State Abbreviation: {StateAbbv}", city.MostSpecificSubdivision.IsoCode);
-
-            _logger.LogInformation("City Name: {City}", city.City.Name);
-
-            _logger.LogInformation("Postal Code: {PostalCode}", city.Postal.Code);
-
-            _logger.LogInformation("Latitude: {Latitude}", city.Location.Latitude); 
-            _logger.LogInformation("Longitude: {Longitude}", city.Location.Longitude);
-
             return new OkResult();
         }
     }
